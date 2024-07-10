7d11b6993c5b61c35b95800fcf2c37e8481d024c
Seiji Kamiya <10seiji18@gmail.com>
2024-07-10 20:13:37 +0900

    Add SQL to queries.py

--- 03_databases_and_sql/01_challenge/queries.py
+++ 03_databases_and_sql/01_challenge/queries.py
@@ -9,35 +9,44 @@ db = conn.cursor()
 
 # アーティストの数
 def number_of_artists(db):
-    query = ""  # ここにSQLクエリを書いてください
+    query = "SELECT count(*) FROM artists;"
     db.execute(query)
     results = db.fetchall()
-    return results
+    return results[0][0]
 
 # アーティストのリスト
 def list_of_artists(db):
-    query = ""  # ここにSQLクエリを書いてください
+    query = "SELECT Name FROM artists;"
     db.execute(query)
     results = db.fetchall()
+    results = [artist[0] for artist in results]
     return results
 
 # 「愛」をテーマにしたアルバムのリスト
 def albums_about_love(db):
-    query = ""  # ここにSQLクエリを書いてください
+    query = "SELECT Title FROM albums WHERE Title LIKE '%love%' ORDER BY Title;"
     db.execute(query)
     results = db.fetchall()
+    results = [artist[0] for artist in results]
     return results
 
 # 指定された再生時間よりも長い楽曲数
 def tracks_longer_than(db, duration):
-    query = ""  # ここにSQLクエリを書いてください
+    query = f"SELECT count(TrackID) FROM tracks WHERE Milliseconds > {duration};"
     db.execute(query)
     results = db.fetchall()
-    return results
+    return results[0][0]
 
 # 最も楽曲数が多いジャンルのリスト
 def genres_with_most_tracks(db):
-    query = ""  # ここにSQLクエリを書いてください
+    query = """
+        SELECT genres.Name, count(Title) as TitleCount
+        FROM tracks
+        join genres on tracks.GenreId == genres.GenreId
+        join albums on tracks.AlbumId == albums.AlbumId
+        GROUP BY genres.Name
+        ORDER BY TitleCount DESC;
+    """
     db.execute(query)
     results = db.fetchall()
     return results
