511ff789abfc8eff6e47a49b70d2c2c9696f5b8d
Seiji Kamiya <10seiji18@gmail.com>
2024-07-18 21:21:14 +0900

    Modify queries.py for challenge

--- 04_sql_and_python/02_challenge/queries.py
+++ 04_sql_and_python/02_challenge/queries.py
@@ -13,7 +13,7 @@ def query_orders(db):
     戻り値:
         注文を表すタプルのリスト
     """
-    query = ""  # ここにSQLクエリを書いてください
+    query = "SELECT * FROM Orders ORDER by OrderID"  # ここにSQLクエリを書いてください
     results = db.execute(query)
     results = results.fetchall()
     return results
@@ -33,7 +33,7 @@ def get_orders_range(db, date_from, date_to):
     戻り値:
         注文を表すタプルのリスト
     """
-    query = ""  # ここにSQLクエリを書いてください
+    query = f"SELECT * FROM Orders WHERE OrderDate >= '{date_from}' AND OrderDate < '{date_to}' ORDER by OrderDate"  # ここにSQLクエリを書いてください
     results = db.execute(query)
     results = results.fetchall()
     return results
@@ -50,32 +50,36 @@ def get_order_details(db):
     戻り値:
         注文の詳細情報を表すタプルのリスト
     """
-    query = ""  # ここにSQLクエリを書いてください
+    query = """
+    SELECT OrderID, ProductName, Quantity FROM OrderDetails 
+    JOIN Products ON OrderDetails.ProductID AND Products.ProductID 
+    ORDER BY OrderID
+    """
     results = db.execute(query)
     results = results.fetchall()
     return results
 
 def main():
     # SQLiteデータベースに接続
-    conn = sqlite3.connect('northwind.db')
+    conn = sqlite3.connect('../data/northwind.db')
 
     # query_orders関数のテスト
     print("All Orders:")
     # 必要に応じて出力の形式を変更してください
     for order in query_orders(conn):
-        print(order)
+        print(f"OrderID: {order[0]}, CustomerID: {order[1]}, EmployeeID: {order[2]}, OrderDate: {order[3]}, ShipperID: {order[4]}")
 
     # get_orders_range関数のテスト
     print("\nOrders in a specified date range:")
     # データベースにある実際の日付に置き換えてください
     for order in get_orders_range(conn, '1996-07-04', '1996-07-10'):
-        print(order)
+        print(f"OrderID: {order[0]}, CustomerID: {order[1]}, EmployeeID: {order[2]}, OrderDate: {order[3]}, ShipperID: {order[4]}")
 
     # get_order_details関数のテスト
     print("\nOrder Details:")
     # 必要に応じて出力の形式を変更してください
     for detail in get_order_details(conn):
-        print(detail)
+        print(f"OrderID: {detail[0]}, ProductName: {detail[1]}, Quantiry: {detail[2]}")
 
     # データベース接続を閉じる
     conn.close()
